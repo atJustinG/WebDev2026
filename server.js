57316bea125b8da06c87515e0a297b72f067987d
atJustinG <45684351+atJustinG@users.noreply.github.com>
2026-06-28 14:04:26 +0200

    Add Feature Server.js Backend implemented
    Add Features Connect to mongodb server,
    create User tables if empty

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const server = express();
+const app = express();
+const multer = require('multer');
+const {MongoClient, ObjectId} = require('mongodb');
+const fs = require('fs');
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
+
+let db;
 
 
-server.use(express.static("public"));
 
-server.listen(8080, () => {
-    console.log("Server is running on 8080");
-});
+async function connectDB() {
+    const client = new MongoClient(MONGO_URI);
+    await client.connect();
+    db = client.db('DATENBANKNAME');
+    console.log('MongoDB verbunden');
+    await seedUsers();
+}
+
+async function seedUsers() {
+    const col = db.collection('users');
+    if(await col.countDocuments() === 0){
+        await col.insertMany([
+            { username: 'admin', password: 'password', name: 'Mina', role: 'admin' },
+            { username: 'guest', password: 'password', name: 'Norman', role: 'user' }
+        ]);
+        console.log('Nutzer angelegt');
+    }
+}
+
+connectDB().then(()=> {
+    app.listen(PORT, () => console.log(`Server läuft auf http://localhost:${PORT}`));
+    }).catch(err =>{
+        console.log('MongoDB Fehler:', err.message);
+    });
+
+
 
